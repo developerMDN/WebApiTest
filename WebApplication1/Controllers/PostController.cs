b10337362a34a360379a951ef80ea84bfd80a5e8
John Rodriguez <developer.jn@gmail.com>
2019-08-08 00:13:29 -0500

    api rest con objetos post para pruebas

---
+++ WebApplication1/Controllers/PostController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("api/post")]
+    public class PostController : ApiController
+    {
+        List<Post> posts = new List<Post>();
+
+        public PostController()
+        {
+            posts = Load.LoadData();
+        }
+
+        [Route("")]
+        public IEnumerable<Post> Get()
+        {
+            return posts;
+        }
+
+        [Route("{id:int:min(1)}")]
+        public IHttpActionResult Get(int id)
+        {
+            Post post = posts.FirstOrDefault<Post>(p => p.Id.Equals(id));
+
+            if (post == null)
+                return NotFound();
+
+            return Ok(post);
+
+        }
+
+        [Route("{title}")]
+        public IEnumerable<Post> Get(string title)
+        {
+            Post[] posts = this.posts
+                .Where<Post>(p => p.Title.Contains(title))
+                .ToArray<Post>();
+
+            return posts;
+
+        }
+
+        [Route("")]
+        public IEnumerable<Post> Post([FromBody] Post newPost)
+        {
+            List<Post> posts = this.posts.ToList<Post>();
+            newPost.Id = posts.Count + 1;
+            posts.Add(newPost);
+            this.posts = posts;
+
+            return this.posts;
+
+        }
+
+
+    }
+}
---
+++ WebApplication1/Models/Post.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Post
+    {
+        public int UserId { get; set; }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool Completed { get; set; }
+    }
+
+    public static class Load
+    {
+
+        static string lorem = @"
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+";
+        public static List<Post> LoadData()
+        {
+            List<Post> posts = new List<Post>();
+            Random rnd = new Random();
+
+            string[] titles = lorem.Split(' ');
+            string statemen = getStatenment(titles);
+            int id = 1;
+
+            for (int n = 1; n <= 50; n++)
+            {
+                if ((n % 10) == 0)
+                    id = id + 1;
+
+                posts.Add(new Post
+                {
+                    Completed = rnd.Next(0, 2) == 1,
+                    Id = n,
+                    Title = getStatenment(titles),
+                    UserId = id
+                });
+
+            }
+
+            return posts;
+        }
+
+        static string getStatenment(string[] titles)
+        {
+            System.Threading.Thread.Sleep(100);
+            Random rnd = new Random();
+            int countTitles = titles.Length - 5;
+            int index = rnd.Next(0, countTitles);
+            int countStatements = rnd.Next(3, 6);
+
+            return string.Join(" ", titles.Skip(index).Take(countStatements));
+        }
+    }
+}
